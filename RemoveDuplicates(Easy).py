71df2e5c309d3d10eefe6d69e6998574a19da7ec
jrwlx <61565241+jrwlx@users.noreply.github.com>
2021-08-05 15:25:25 -0700

    Two Pointers

--- RemoveDuplicates(Easy).py
+++ RemoveDuplicates(Easy).py
@@ -1,3 +1,11 @@
 
-def func(nums):
-    
+def remove_duplicates(nums):
+    next_nonduplicate = 1
+    i = 1
+    while i < len(nums):
+        if nums[next_nonduplicate - 1] != nums[i]:
+            nums[next_nonduplicate] = nums[i]
+            next_nonduplicate += 1
+        i += 1
+    return next_nonduplicate
+
